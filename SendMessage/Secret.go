de3222c5d6970329fee939387c24eb51bb5e7b08
SmartLyu <43705409+SmartLyu@users.noreply.github.com>
2019-05-09 14:17:36 +0800

    Update Secret.go

--- SendMessage/Secret.go
+++ SendMessage/Secret.go
@@ -1,8 +1,8 @@
 package main
 
 var SecretId = corpText{
-	corpid:     "ww77f7483dada51439",
-	corpsecret: "_ebRca-EqhFei9OKhR0l2k2--dHFANx-1qfdVnh70Kg",
+	corpid:     "",
+	corpsecret: "",
 	touser:     "@all",
 	agentid:    1000002,
 	content:    "Test Api",
