b85f9276ed9a4787848e3e6406d378c8492d9bfe
SmartLyu <luyu151111@163.com>
2019-05-09 14:07:42 +0800

    change main

---
+++ SendMessage/Secret.go
@@ -0,0 +1,10 @@
+package main
+
+var SecretId = corpText{
+	corpid:     "ww77f7483dada51439",
+	corpsecret: "_ebRca-EqhFei9OKhR0l2k2--dHFANx-1qfdVnh70Kg",
+	touser:     "@all",
+	agentid:    1000002,
+	content:    "Test Api",
+	safe:       "1",
+}
