c2f133b9b55b33e24d47d3567db91c67bb34d6ae
SmartLyu <43705409+SmartLyu@users.noreply.github.com>
2020-01-14 13:54:38 +0800

    Update Secret.go

--- Patrol/PatrolServer/CallPolice/Secret.go
+++ Patrol/PatrolServer/CallPolice/Secret.go
@@ -1,22 +1,19 @@
 package CallPolice
 
 var SecretId = corpText{
-	corpid:     "wwd0b64b830dd50312",
-	corpsecret: "Mebw1BA1cZBY17FrGJcRJniaGWt_LCQe_DGyPLDznRI",
+	corpid:     "",
+	corpsecret: "",
 	// 报警发给运维组、业务组
 	toparty: "2|11",
-
-	// 用于巡查测试使用的发送对象
-	//touser:  "YuZhiYuan|HuangZheWei01",
 	agentid: 1000002,
 	content: "Error Api",
 	safe:    "1",
 }
 
 var MessageId = corpText{
-	corpid:     "wwd0b64b830dd50312",
-	corpsecret: "Mebw1BA1cZBY17FrGJcRJniaGWt_LCQe_DGyPLDznRI",
-	touser:     "YuZhiYuan|HuangZheWei01",
+	corpid:     "",
+	corpsecret: "",
+	touser:     "",
 	agentid:    1000002,
 	content:    "Error Api",
 	safe:       "1",
